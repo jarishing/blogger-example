e29b4cef968bb11d727c61b08bbc6a6731fb7f54
sugurutakeuchi <k013a2065@gmail.com>
2025-07-31 17:20:30 +0900

    eslintrc fix

--- packages/types/.eslintrc.js
+++ packages/types/.eslintrc.js
@@ -3,6 +3,6 @@ const path = require('path');
 module.exports = {
   extends: [path.resolve(__dirname, '../tooling/eslint/base.js')],
   parserOptions: {
-    project: './tsconfig.json'
+    project: path.resolve(__dirname, './tsconfig.json')
   }
 };
